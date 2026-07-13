be25b98d289f6bfa7fbd7043a623a7166b4dd4eb
Shahzor Ali <mhoibshahzor@gmail.com>
2026-07-13 20:23:18 +0500

    Make sidebar always visible with responsive sizing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,9 +9,9 @@ export default function DashboardLayout({
   return (
     <div className="flex min-h-screen bg-black">
       <Sidebar />
-      <div className="flex flex-1 flex-col">
+      <div className="flex flex-1 flex-col min-w-0">
         <Navbar />
-        <main className="flex-1 p-6">{children}</main>
+        <main className="flex-1 p-3 sm:p-4 md:p-6">{children}</main>
       </div>
     </div>
   );
